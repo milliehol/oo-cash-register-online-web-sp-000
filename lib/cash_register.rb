f214c5354b3963354a57b96d822a7611d909af58
Carla Holcomb <carla.programmer@gmail.com>
2019-12-01 23:44:17 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,7 +19,7 @@ class CashRegister
 
   def add_item(title, price, quantity = 1)
     @price = price
-    @total += (price*quantity)
+    @total += (@price*quantity)
     @@total << @total
   end
 
