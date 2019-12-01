ee6215e2b08a66e3c55045a3d38deac4b450f1c2
Carla Holcomb <carla.programmer@gmail.com>
2019-12-01 23:42:26 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+class CashRegister
 
+  attr_accessor :amount, :discount, :name, :total
+
+  @@total = []
+
+  def initialize(discount = 0)
+    @@all = []
+    @discount = discount
+    @total = 0
+  end
+
+  def total
+    @@total.each do |total|
+      @total += total
+    end
+      @total
+  end
+
+  def add_item(title, price, quantity = 1)
+    @price = price
+    @total += (price*quantity)
+    @@total << @total
+
+
+  end
+
+  def void_last_transaction
+     @total -= @price
+  end
+end
